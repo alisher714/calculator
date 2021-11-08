eaba78dfb214b4ce2cb6f0964f51aa014e20ebfa
alisher714 <alishermalik1100@gmail.com>
2021-11-08 19:49:43 +0500

    change the name of variables

--- calculator_using_Switch.cpp
+++ calculator_using_Switch.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 main()
 {
-	int a,y ;
-	float b, c;
+	int choice,again ;
+	float num1, num2;
 	do{
 	
 		
@@ -11,36 +11,36 @@ main()
 	cout<<"enter 2 for subtraction :"<<endl;
 	cout<<"enter 3 for multiplication :"<<endl;
 	cout<<"enter 4 for division :"<<endl;
-	cin>>a;
+	cin>>choice;
 	
 	cout<<"enter values ";
-	cin>>b>>c;
+	cin>>num1>>num2;
 	
-	switch(a)
+	switch(choice)
 	{
 		case 1:
-			cout<<"the sum is :"<<b+c<<endl<<endl;
+			cout<<"the sum is :"<<num1+num2<<endl<<endl;
 			break;
 		
 		case 2:
-			cout<<"the difference is :"<<b-c<<endl<<endl;
+			cout<<"the difference is :"<<num1-num2<<endl<<endl;
 			break;
 		
 		case 3:
-			cout<<"the product is :"<<b*c<<endl<<endl;
+			cout<<"the product is :"<<num1*num2<<endl<<endl;
 			break;
 		
 		case 4:
-			cout<<"the division is :"<<b/c<<endl<<endl;
+			cout<<"the division is :"<<num1/num2<<endl<<endl;
 			break;
 		
 		default :
 			cout<<"the num is invalid please enter num between 1-4 "<<endl;}
 			
 	cout<<"enter 0 to rerun the program other wise press any key to terminate the program :"<<endl;
-	cin>>y;				
+	cin>>again;				
 							
-	}while(y==0);
+	}while(again==0);
 	cout<<"the program is finished ";
 	
 	
